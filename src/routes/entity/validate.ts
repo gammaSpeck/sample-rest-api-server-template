759dae95a37cb3324f0e2bb35db92dcba90e0398
gammaSpeck <madhukm3141@gmail.com>
2024-06-27 21:48:30 +0530

    Cleaned code

--- src/routes/entity/validate.ts
+++ src/routes/entity/validate.ts
@@ -1,15 +1,6 @@
 import { z } from 'zod'
 
-// creating a schema for strings
 export const UserPayloadSchema = z.object({
   name: z.string().min(2).max(32),
   age: z.number().min(16)
 })
-
-// // parsing
-// mySchema.parse("tuna"); // => "tuna"
-// mySchema.parse(12); // => throws ZodError
-
-// // "safe" parsing (doesn't throw error if validation fails)
-// mySchema.safeParse("tuna"); // => { success: true; data: "tuna" }
-// mySchema.safeParse(12); // => { success: false; error: ZodError }
